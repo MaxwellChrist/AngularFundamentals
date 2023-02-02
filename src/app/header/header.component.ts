7e9557edb0c441b14a16906af2a0badf17f27543
MaxwellChrist <ribthulu@gmail.com>
2023-02-02 16:55:04 -0500

    added header and footer

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,15 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   template: `
-  <div class="navbar is-link">
+  <div class="navbar">
     <div class="navbar-brand">
       <a class="navbar-item">
-        Logo
+        <img src="assets/images/GoldStandard.png" class="image"/>
       </a>
     </div>
   </div>
   `,
-  styles: []
+  styles: [`
+    .navbar 
+      background-color: #EAE9E0
+
+    .navbar-item 
+      padding: 0 0 0 0.75rem
+    
+`]
 })
 export class HeaderComponent implements OnInit {
   constructor() {}
