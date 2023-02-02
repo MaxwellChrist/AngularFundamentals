343069562c7cb81c9557a8960a1fce23dab09983
MaxwellChrist <ribthulu@gmail.com>
2023-02-02 12:43:38 -0500

    added bulma to project to help with styling, created header and footer components

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  template: `
+  <footer class="footer">
+    <div class="container content has-text-centered">
+      <p>Made by Max Christ</p>
+    </div>
+  </footer>
+  `,
+  styles: []
+})
+export class FooterComponent {
+
+}
