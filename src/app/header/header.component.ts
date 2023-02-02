5bd0a1698e0e8052dcb65a2ea5ff57dbf45a4d6a
MaxwellChrist <ribthulu@gmail.com>
2023-02-02 17:13:38 -0500

    fixed logo

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,18 +5,21 @@ import { Component, OnInit } from '@angular/core';
   template: `
   <div class="navbar">
     <div class="navbar-brand">
-      <a class="navbar-item">
-        <img src="assets/images/GoldStandard.png" class="image"/>
-      </a>
+      <a class="navbar-item"></a>
     </div>
   </div>
   `,
   styles: [`
     .navbar 
       background-color: #EAE9E0
+      height: 100px
 
     .navbar-item 
-      padding: 0 0 0 0.75rem
+      padding: 0
+      margin: 0 0 0 1rem
+      background: url('../../assets/images/GoldStandard.png') no-repeat center center
+      background-size: cover
+      width: 100px
     
 `]
 })
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  template: `
+    <section class="hero is-bold is-fullheight">
+      <div class="hero-body">
+        <p class="title">
+          My Home Page
+        </p>
+      </div>
+    </section>
+  `,
+  styles: []
+})
+export class HomeComponent {
+
+}
